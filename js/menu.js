1c946b37b1a0a410b0a97bc76d5a5c0bd858fe7e
Mark Yatsiuk <markiatsuk@gmail.com>
2021-11-05 22:18:16 +0200

    мелкие поправки

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.menu-open-btn'),
+    closeMenuBtn: document.querySelector('.close-button'),
+    menu: document.querySelector('.backdrop'),
+    menuItem: document.querySelector('.mob-menu')
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menu.addEventListener('click', removeMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+  function removeMenu(e) {
+    if (e.target === refs.menu) {
+      refs.menu.classList.add('is-hidden');
+    }
+  }
+})();
+
